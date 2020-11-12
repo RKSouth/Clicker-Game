0f3878204240900315f114482a2008f380a3a44f
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-11 22:59:51 -0800

    need to fix current index id problem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -34,30 +34,31 @@ class App extends Component {
 // }
 
 
-shuffleData= () => {
-  let i = this.state.data;
-  for (; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = data[i];
-    data[i] = data[j];
-    data[j] = temp;
-  }
-  return data;
-}
-
-// shuffleData = (data) => {
-//     let index = data.length -1;
-//     let temp;
-//     let random;
-//     while (index > 0) {
-//         const random = Math.floor(Math.random()*(index+1))
-//         const temp = data[index]
-//         data[index] = data[random]
-//         data[random]= temp
-//         index--
-//     }
-//     return data
+// shuffleData= () => {
+//   let i = this.state.data;
+//   for (; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     const temp = this.state.data[i];
+//     this.state.data[i] = this.state.data[j];
+//     this.state.data[j] = temp;
+//   }
+//   console.log("shuffle")
+//   return this.state.data;
 // }
+
+shuffleData = (data) => {
+    let index = 11;
+    let temp;
+    let random;
+    while (index > 0) {
+        random = Math.floor(Math.random()*(index+1))
+        temp = data[index]
+        data[index] = data[random]
+        data[random]= temp
+        index--
+    }
+    return data
+}
   handleCorrect = () => {
     
     if (this.state.score+1 > this.state.topScore) {
@@ -70,24 +71,25 @@ shuffleData= () => {
     }
   }
 
-  handleReset = (currentClickedItem) => {
-    //if current score is at max reset score to 0 and topscore to 0
-    if (this.state.score+1 === this.state.maxScore) {
-      this.setState({score: 0, topScore: 0})
-      //reset clicked state for ClickedItem
-      const updatedClickedItem = currentClickedItem.map(ch => (true) ? { ...ch, isClicked: false } : ch)
-      return updatedClickedItem
-    }else{
-      return currentClickedItem
-    }
-  }
+  // handleReset = (currentClickedItem) => {
+  //   //if current score is at max reset score to 0 and topscore to 0
+  //   if (this.state.score+1 === this.state.maxScore) {
+  //     this.setState({score: 0, topScore: 0})
+  //     //reset clicked state for ClickedItem
+  //     const updatedClickedItem = currentClickedItem.map(ch => (true) ? { ...ch, isClicked: false } : ch)
+  //     return updatedClickedItem
+  //   }else{
+  //     return currentClickedItem
+  //   }
+  // }
 
   handleIncorrect = () => {
     //incorrect selection made, reset score to 0
     this.setState({score: 0})
     //reset clicked state for ClickedItem
-    const updatedClickedItem = this.state.ClickedItem.map(ch => ch.isClicked === true ? { ...ch, isClicked: false } : ch)
-    return updatedClickedItem
+    this.state.data.forEach(element => {
+    element.clicked = "false";
+    }); 
   };
 
 
@@ -98,35 +100,26 @@ shuffleData= () => {
     const topScore = 0;
 
     console.log("click");
-    console.log(id);
-    console.log(id.target.alt);
+    console.log(currentClickedItem);
     console.log(this.state.data)
-    console.log(currentClickedItem)
+    console.log(this.state.data[currentClickedItem[0].id].clicked)
 
-     if (ClickItem.clicked === "false"){
-       console.log("I've been clicked")
+     if (this.state.data[currentClickedItem[0].id].clicked === "false"){
+       console.log("I've been clicked for the very first time (like a virgin)")
+       this.state.data[currentClickedItem[0].id].clicked = "true";
+       this.handleCorrect();
      }
-    
-    if (!this.state) {
-      // if score and topScore are the same, then there is a new topScore value
-      if (score === topScore) {
-        score++;
-        topScore++;
-        
+     else{
+       this.handleIncorrect();
+     }
 
-      } else {
-        score++;
-       
+     this.setState(
+      {
+          data: this.shuffleData(this.state.data)
       }
- 
-    this.setState({
-      data: this.shuffleData(this.state.data),
-      img: "",
-      name: "",
-      id: ""
-    });
+  )
   };
-}
+
 
   
 
